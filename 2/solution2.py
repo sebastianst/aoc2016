3dbd150626ee7e60f316787a44caf128502906c1
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2016-12-02 17:02:34 +0100

    [2] Compactified 2nd solution. Fancier and cryptic pad generation.

--- 2/solution2.py
+++ 2/solution2.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python
-input = open('input', 'r')
-
 x,y,code = -2,0,''
 
-pad = {(0,-2): 1,
-        (-1,-1): 2, (0,-1): 3, (1,-1): 4,
-        (-1,1): 'A', (0,1): 'B', (1,1): 'C',
-        (0,2): 'D'}
-pad.update({(i,0): i+7 for i in range(-2,3)}) # middle row
+pad = {(x,y): '{:X}'.format(x+o) for x in range(-2,3)
+        for (y,o) in [(-2,1),(-1,3),(0,7),(1,11),(2,13)]}
 
-for line in input:
+for line in open('input', 'r'):
     for m in line:
-        if m == 'U':
-            y -= (y-abs(x) > -2)
-        elif m == 'D':
-            y += (y+abs(x) < 2)
-        elif m == 'L':
-            x -= (x-abs(y) > -2)
-        elif m == 'R':
-            x += (x+abs(y) < 2)
-    code += str(pad[(x,y)])
+        if   m == 'U': y -= (y-abs(x) > -2)
+        elif m == 'D': y += (y+abs(x) < 2)
+        elif m == 'L': x -= (x-abs(y) > -2)
+        elif m == 'R': x += (x+abs(y) < 2)
+    code += pad[(x,y)]
 
 print(code)
