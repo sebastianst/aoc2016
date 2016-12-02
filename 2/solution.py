40c9b51d7258d97fb0b4bf1ded1f58a1e3535105
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2016-12-02 16:39:22 +0100

    [1] compactify solution

--- 2/solution.py
+++ 2/solution.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
-input = open('input', 'r')
-
 x,y,code = 2,1,''
 
-def pad(x,y):
-    return x+3*y
-
-for line in input:
+for line in open('input', 'r'):
     for m in line:
-        if m == 'U':
-            y -= (y>0)*1
-        elif m == 'D':
-            y += (y<2)*1
-        elif m == 'L':
-            x -= (x>1)*1
-        elif m == 'R':
-            x += (x<3)*1
-    code += str(pad(x,y))
+        if   m == 'U': y -= (y>0)
+        elif m == 'D': y += (y<2)
+        elif m == 'L': x -= (x>1)
+        elif m == 'R': x += (x<3)
+    code += str(x+3*y)
 
 print(code)
