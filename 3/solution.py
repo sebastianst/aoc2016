9af265fdf3e15f2fdce78618db85c1b8e5d78fb9
Sebastian Stammler <stammler.s@gmail.com>
2016-12-03 13:46:31 +0100

    [3] compactify

--- 3/solution.py
+++ 3/solution.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 n = 0
 for line in open('input', 'r'):
-    a = [int(x) for x in line.split()]
-    p = True
-    for i in range(3):
-        p &= (a[i%3] < a[(i+1)%3] + a[(i+2)%3])
-    n += p
+    t = [int(x) for x in line.split()]
+    n += all([(t[i%3] < t[(i+1)%3] + t[(i+2)%3]) for i in range(3)])
 
 print(n)
--- 3/solution2.py
+++ 3/solution2.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
-n,r = 0,0
-a,b,c = [0,0,0],[0,0,0],[0,0,0]
-def valid(a):
-    p = True
-    for i in range(3):
-        p &= (a[i%3] < a[(i+1)%3] + a[(i+2)%3])
-    return p
+n,r,a = 0,0,[[0]*3]*3
+valid = lambda t: all([(t[i%3] < t[(i+1)%3] + t[(i+2)%3]) for i in range(3)])
 
 for line in open('input', 'r'):
-    a[r], b[r], c[r] = [int(x) for x in line.split()]
+    a[r] = [int(x) for x in line.split()]
     r = (r+1) % 3
-    if not r: n += sum(map(valid,[a,b,c]))
+    if not r: n += sum(map(valid,zip(*a)))
 
 print(n)
