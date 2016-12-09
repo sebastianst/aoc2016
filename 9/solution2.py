55aeee6495539b8111a21ef9a8c85c42a9f2e4ad
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2016-12-09 20:17:27 +0100

    [9] use str.find in second solution

--- 9/solution2.py
+++ 9/solution2.py
@@ -3,16 +3,17 @@ import re
 def decompress_len(inp):
     n = 0
     while len(inp):
-        c=inp[0]
-        if c != '(':
-            n += 1
-            inp = inp[1:]
-        else:
-            a, b = re.match(r'\((\d+)x(\d+)\)', inp).groups()
-            l = 3+len(a)+len(b)
-            inp = inp[l:]
-            n += int(b) * decompress_len(inp[:int(a)])
-            inp = inp[int(a):]
+        i = inp.find('(')
+        if i == -1:
+            n += len(inp)
+            break
+        n += i
+        inp = inp[i:]
+        a, b = re.match(r'\((\d+)x(\d+)\)', inp).groups()
+        l = 3+len(a)+len(b)
+        inp = inp[l:]
+        n += int(b) * decompress_len(inp[:int(a)])
+        inp = inp[int(a):]
 
     return n
 
