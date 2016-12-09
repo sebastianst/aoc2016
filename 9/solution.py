15f4b8a7f510d61f8cbdfa3217c26ca07a289cb1
Sebastian Stammler <stammler.s@gmail.com>
2016-12-09 13:03:38 +0100

    [9] Partial solution. Not working yet.

---
+++ 9/solution.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import re
+output = ''
+inp = open('input', 'r').readline()
+while len(inp):
+    c=inp[0]
+    if c != '(':
+        output += c
+        inp = inp[1:]
+    else:
+        a, b = re.match(r'\((\d+)x(\d+)\)', inp).groups()
+        l = 3+len(a)+len(b)
+        inp = inp[l:]
+        output += int(b) * inp[:int(a)]
+        inp = inp[int(a):]
+
+print(len(output))
+# 404515 70187 too high
