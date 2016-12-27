1ce938d5e74d923e02648924368616320459b433
Sebastian Stammler <stammler.s@gmail.com>
2016-12-27 14:13:54 +0100

    [18] Second solution. Compacter algorithm.

--- 18/solution.py
+++ 18/solution.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 a, n = open('input', 'r').readline()[:-1], 0
-W, a = len(a), [x=='^' for x in a]
+W, a = len(a), [x=='^' for x in '.'+a+'.']
 
 def trap(a, i):
     c = a[i:i+3]
     return (sum(c)==2 and c[1]) or (sum(c)==1 and not c[1])
 
-for _ in range(40):
+for _ in range(400000):
     n += W - sum(a)
-    a = [False] + a + [False]
-    a = [trap(a,i) for i in range(W)]
+    a = [False] + [trap(a,i) for i in range(W)] + [False]
 
 print(n)
